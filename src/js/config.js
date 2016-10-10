ec3a4fd4780b9c4f72e097633c79ddc3f700b28a
贺贤娟 <1432109032@qq.com>
2016-10-11 01:03:12 +0800

    test

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 var corpId = "ding837885825c9bc4b3";
 var agentId = '43790978';
-var timeStamp = '1440678945';
+var timeStamp = '1440678946';
 var nonceStr = 'abcdefg';
 var apiUrl = "https://oapi.dingtalk.com/";
 var access_token = '693c9927b61f37058ab03b41c129baff';
