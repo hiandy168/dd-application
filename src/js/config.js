1010cf9acb30b8813e2f2ebb84f6212caebc6fb4
贺贤娟 <1432109032@qq.com>
2016-10-10 23:38:56 +0800

    获取接口数据

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,6 @@ var corpId = "ding837885825c9bc4b3";
 var agentId = '43790978';
 var timeStamp = '1440678945';
 var nonceStr = 'abcdefg';
-var jsapi_ticket = "47uOWz8rYYkpUyJXBPazCZnmD480y9TRgFtmJG02cxYXflMxQHq3wktrndiX0jXRvGRi8pOw9MLzT3dnQv906X";
+var apiUrl = "https://oapi.dingtalk.com/";
+var access_token = '693c9927b61f37058ab03b41c129baff';
 
