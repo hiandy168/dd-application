c327c3df7b134e55d2a5012463ec8ebc57a64412
贺贤娟 <1432109032@qq.com>
2016-10-10 23:25:17 +0800

    test

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@ var corpId = "ding837885825c9bc4b3";
 var agentId = '43790978';
 var timeStamp = '1440678945';
 var nonceStr = 'abcdefg';
-var jsapi_ticket = "CQDQYK83hGIkC0dZDx9FO167GvUXCUbUzT8BxxteJGfsm9iJFykjDTSy3pqLe8zqZ2wvBBF2WRS6cHLNRcmWkT";
+var jsapi_ticket = "47uOWz8rYYkpUyJXBPazCZnmD480y9TRgFtmJG02cxYXflMxQHq3wktrndiX0jXRvGRi8pOw9MLzT3dnQv906X";
 
