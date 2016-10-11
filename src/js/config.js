4badecbc23ab7c14dc823624cd8fee75a985b907
贺贤娟 <1432109032@qq.com>
2016-10-11 10:07:59 +0800

    test

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@ var agentId = '43790978';
 var timeStamp = '1440678946';
 var nonceStr = 'abcdefg';
 var apiUrl = "https://oapi.dingtalk.com/";
-var access_token = '693c9927b61f37058ab03b41c129baff';
+var access_token = 'bf4c872d1c5b32cf8a23fc2d57f0f52d';
 
